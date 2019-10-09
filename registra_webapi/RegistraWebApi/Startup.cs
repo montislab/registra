2b5d39301ea52518bfddeb5a940597629f20079f
Marcin Balut <marcinbalutpoland@gmail.com>
2019-10-09 17:26:45 +0200

    add connection string configuration

--- registra_webapi/RegistraWebApi/Startup.cs
+++ registra_webapi/RegistraWebApi/Startup.cs
@@ -27,7 +27,7 @@ namespace RegistraWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<RegistraDbContext>(options => options.UseSqlServer(""));
+            services.AddDbContext<RegistraDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("RegistraConnectionStringDev")));
             services.AddControllers();
         }
 
