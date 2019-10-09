48f7f1c75603b3699b29a5f731c031e6e578b2dd
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-10-09 16:58:14 +0200

    Add RegistraDbContext

---
+++ registra_webapi/RegistraWebApi/Persistance/RegistraDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistraWebApi.Persistance
+{
+    public class RegistraDbContext : DbContext
+    {
+        public RegistraDbContext(DbContextOptions<RegistraDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
