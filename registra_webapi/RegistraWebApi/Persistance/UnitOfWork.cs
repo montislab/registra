63436b615538c08c738cb37f834f3c58c79a564f
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-10-09 16:45:35 +0200

    Add repository pattern scaffold

---
+++ registra_webapi/RegistraWebApi/Persistance/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistraWebApi.Persistance
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly object context;
+
+        public UnitOfWork(object context)
+        {
+            this.context = context;
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
