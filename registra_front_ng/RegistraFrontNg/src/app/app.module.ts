e849b60b28e6cee66090e7787135808da6578699
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-10-23 18:31:15 +0200

    Implement of logging mechanism in angular app

--- registra_front_ng/RegistraFrontNg/src/app/app.module.ts
+++ registra_front_ng/RegistraFrontNg/src/app/app.module.ts
@@ -1,22 +1,31 @@
+import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllClientListComponent } from './all-client-list/all-client-list.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AllClientListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      AllClientListComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
