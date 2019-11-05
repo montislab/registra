11b4ac211ff4ff715d41bde0b0700eb27ec1fc9f
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-11-05 21:53:47 +0100

    Create test project for RegistraWebApi

---
+++ RegistraWebApiTests/Example.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace RegistraWebApiTests
+{
+    public class Example
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact(Skip = "Fail for example")]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
