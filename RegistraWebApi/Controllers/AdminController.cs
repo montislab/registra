e4a95f4987abdc69d6fc6b468a324ca9b4d213de
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-11-05 11:27:29 +0100

    Prepare list of users with role in API

---
+++ RegistraWebApi/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RegistraWebApi.Dtos;
+using RegistraWebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistraWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly UserManager<User> userManager;
+
+        public AdminController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("usersWithRoles")]
+        public async Task<IActionResult> GetUsersWithRoles() => Ok(await PrepareUsersWithRoles());
+
+        private async Task<List<UserWithRolesDto>> PrepareUsersWithRoles()
+        {
+            return await userManager.Users
+                .OrderBy(u => u.Id)
+                .Select(user => new UserWithRolesDto
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Roles = userManager.GetRolesAsync(user).Result
+                }).ToListAsync();
+        }
+    }
+}
---
+++ RegistraWebApi/Dtos/UserWithRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistraWebApi.Dtos
+{
+    public class UserWithRolesDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public IList<string> Roles { get; set; } = new List<string>();
+    }
+}
