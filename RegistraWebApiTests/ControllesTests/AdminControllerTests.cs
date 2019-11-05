655555cee262f00a1ddc64f596ff03f02f955220
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-11-05 22:10:55 +0100

    Add reference to RegistraWebApi in test project and write some simple test

---
+++ RegistraWebApiTests/ControllesTests/AdminControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RegistraWebApi.Controllers;
+using RegistraWebApi.Models;
+using RegistraWebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RegistraWebApiTests.ControllesTests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void AdminControled_IsJustCreated()
+        {
+            IAdminService adminService = null;  //TODO: create real service
+            AdminController adminController = new AdminController(adminService);
+
+            Assert.NotNull(adminController);
+        }
+    }
+}
