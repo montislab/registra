b9061b91d0c953d04536481e959155c2746256e7
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-11-06 11:50:09 +0100

    Add Moq package to RegistraWebApi project and use it in Admin Controller test as an example

--- RegistraWebApiTests/ControllesTests/AdminControllerTests.cs
+++ RegistraWebApiTests/ControllesTests/AdminControllerTests.cs
@@ -3,10 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using RegistraWebApi.Controllers;
 using RegistraWebApi.Models;
 using RegistraWebApi.Services;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
+using Moq;
 
 namespace RegistraWebApiTests.ControllesTests
 {
@@ -15,12 +13,34 @@ namespace RegistraWebApiTests.ControllesTests
         [Fact]
         public void AdminControled_IsJustCreated()
         {
-            IAdminService adminService = null;  //TODO: create real service
-            AdminController adminController = new AdminController(adminService);
+            Mock<UserManager<User>> userManagerMock = PrepareUserManagerMock();
+            Mock<RoleManager<Role>> roleManagerMock = PrepareRoleManagerMock();
+
+            Mock<AdminService> adminService = new Mock<AdminService>(userManagerMock.Object, roleManagerMock.Object);
+            AdminController adminController = new AdminController(adminService.Object);
 
             adminController
                 .Should()
                 .NotBeNull();
+        }
+
+        private Mock<UserManager<User>> PrepareUserManagerMock()
+        {
+            Mock<IUserStore<User>> userStoreMock = new Mock<IUserStore<User>>();
+            Mock<UserManager<User>> userManagerMock = new Mock<UserManager<User>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
+            userManagerMock.Object.UserValidators.Add(new UserValidator<User>());
+            userManagerMock.Object.PasswordValidators.Add(new PasswordValidator<User>());
+
+            return userManagerMock;
+        }
+
+        private static Mock<RoleManager<Role>> PrepareRoleManagerMock()
+        {
+            var roleStoreMock = new Mock<IRoleStore<Role>>();
+            var roleManagerMock = new Mock<RoleManager<Role>>(roleStoreMock.Object, null, null, null, null);
+
+            return roleManagerMock;
         }
     }
 }
