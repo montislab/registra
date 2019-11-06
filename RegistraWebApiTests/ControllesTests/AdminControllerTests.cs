2ed8900bdf9ee30c7ac4c2a864a60fc50ec59df3
Piotr.Odbierzychleb <piotro3425@gmail.com>
2019-11-06 15:00:45 +0100

    Add tests of Get User With Roles functionality

--- RegistraWebApiTests/ControllesTests/AdminControllerTests.cs
+++ RegistraWebApiTests/ControllesTests/AdminControllerTests.cs
@@ -5,6 +5,11 @@ using RegistraWebApi.Models;
 using RegistraWebApi.Services;
 using Xunit;
 using Moq;
+using RegistraWebApi.Dtos;
+using RegistraWebApi.Constants;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RegistraWebApiTests.ControllesTests
 {
@@ -41,6 +46,80 @@ namespace RegistraWebApiTests.ControllesTests
             var roleManagerMock = new Mock<RoleManager<Role>>(roleStoreMock.Object, null, null, null, null);
 
             return roleManagerMock;
+        }
+
+        [Fact]
+        public void GetUserWithRoles_ReturnsEmptyCollection()
+        {
+            Mock<UserManager<User>> userManagerMock = PrepareUserManagerMock();
+            Mock<RoleManager<Role>> roleManagerMock = PrepareRoleManagerMock();
+
+            Mock<IAdminService> adminService = new Mock<AdminService>(userManagerMock.Object, roleManagerMock.Object)
+                .As<IAdminService>();
+            adminService.Setup(mas => mas.PrepareUsersWithRoles()).Returns(Task.FromResult(new List<UserWithRolesDto>()));
+
+            AdminController adminController = new AdminController(adminService.Object);
+
+            IActionResult result = adminController.GetUsersWithRoles().Result;
+
+            result
+                .Should()
+                .BeOfType<OkObjectResult>();
+
+            OkObjectResult okObjectResult = result as OkObjectResult;
+            okObjectResult.Value
+                .Should()
+                .BeEquivalentTo(new List<UserWithRolesDto>());
+        }
+
+        [Fact]
+        public void GetUserWithRoles_ReturnsListOdUsersWithRoles()
+        {
+            Mock<UserManager<User>> userManagerMock = PrepareUserManagerMock();
+            Mock<RoleManager<Role>> roleManagerMock = PrepareRoleManagerMock();
+
+            Mock<IAdminService> adminService = new Mock<AdminService>(userManagerMock.Object, roleManagerMock.Object)
+                .As<IAdminService>();
+            adminService.Setup(mas => mas.PrepareUsersWithRoles()).Returns(Task.FromResult(PrepareDummyUsersWithRoles()));
+
+            AdminController adminController = new AdminController(adminService.Object);
+
+            IActionResult result = adminController.GetUsersWithRoles().Result;
+
+            result
+                .Should()
+                .BeOfType<OkObjectResult>();
+
+            OkObjectResult okObjectResult = result as OkObjectResult;
+            okObjectResult.Value
+                .Should()
+                .BeEquivalentTo(PrepareDummyUsersWithRoles());
+        }
+
+        private List<UserWithRolesDto> PrepareDummyUsersWithRoles()
+        {
+            return new List<UserWithRolesDto>()
+            {
+                new UserWithRolesDto()
+                {
+                    Id = 1,
+                    UserName = "admin@admin.com",
+                    Roles = new string[]
+                    {
+                        RoleNames.Client,
+                        RoleNames.Admin
+                    }
+                },
+                new UserWithRolesDto()
+                {
+                    Id = 2,
+                    UserName = "asd@asd.com",
+                    Roles = new string[]
+                    {
+                        RoleNames.Client
+                    }
+                }
+            };
         }
     }
 }
