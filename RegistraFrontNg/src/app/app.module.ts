d80ee553101b0b3e815950e3033c3b1b9ab59ce4
Marcin Balut <marcinbalutpoland@gmail.com>
2019-11-07 11:17:10 +0100

    JWT auto attach configuration correction

--- RegistraFrontNg/src/app/app.module.ts
+++ RegistraFrontNg/src/app/app.module.ts
@@ -46,7 +46,7 @@ export function tokenGetter() {
          config: {
             tokenGetter: tokenGetter,
             whitelistedDomains: ['localhost:5000'],
-            blacklistedRoutes: ['localhost:5000/api/auth']
+            blacklistedRoutes: ['localhost:5000/auth']
          }
       }),
       ModalModule.forRoot()
